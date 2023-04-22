b3c4e5bff36bee53fb7af5b7a010840ee73d5560
Marlon Pascual Marrero Arancibia <marlonpmarancibia@gmail.com>
2023-04-22 00:07:03 -0400

    Update save.gd
    Fixed save.gd not saving.

--- menu/scripts/save.gd
+++ menu/scripts/save.gd
@@ -1,9 +1,14 @@
 class_name SaveGame
 extends Resource
 
-const SAVE_PATH:= "user://savegame.tres"
+const SAVE_PATH:= "user://saves/"
+const FILE_NAME = "player.res"
 
 var player: Resource = preload("res://characters/player.tscn")
 
 func write_savegame():
-	ResourceSaver.save(player, SAVE_PATH)
+	verify_filepath()
+	ResourceSaver.save(player, SAVE_PATH+FILE_NAME)
+
+func verify_filepath():
+	DirAccess.make_dir_absolute(SAVE_PATH)
